a4c2c17a1e26d0c7e19082d37fc76eac9065c312
Sarah Battey <sarahsbattey@gmail.com>
2018-04-13 21:29:57 -0600

    initialize repository :tada:

---
+++ app/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import List from './List'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <List />
+      </div>
+    )
+  }
+}
+
+export default App
